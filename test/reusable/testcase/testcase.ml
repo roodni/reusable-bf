887e5c56bcc74b15d0f414083f07fb7513df95bb
rodoni <41773436+roodni@users.noreply.github.com>
2023-03-11 13:21:46 +0900

    ディレクトリ名変更

--- test/reusable/testcase/testcase.ml
+++ test/reusable/testcase/testcase.ml
@@ -15,25 +15,25 @@ let case ?(run_bfi=true) ?(cell_type=Bf.Exe.Overflow256) path io_list =
 
 let cases =
   [
-    case "sample/misc/hoge/rev.bfr"
+    case "sample/misc/bf/rev.bfr"
       [ ("hello\n", "olleh") ];
-    case "sample/misc/hoge/rev2.bfr"
+    case "sample/misc/bf/rev2.bfr"
       [ ("hello\na", "olleh") ];
-    case "sample/misc/hoge/echo.bfr"
+    case "sample/misc/bf/echo.bfr"
       [ ("Hello, world!#test", "Hello, world!#") ];
     case "sample/misc/metalang/hygienic.bfr"
       [ ("", "O") ];
-    case "sample/misc/hoge/rev3.bfr"
+    case "sample/misc/bf/rev3.bfr"
       [ ("hello\na", "olleh") ];
     case "sample/misc/metalang/prime.bfr"
       [ ("", "2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 ")];
-    case "sample/misc/hoge/switch_nat.bfr"
+    case "sample/misc/bf/switch_nat.bfr"
       [ ("0", "B"); ("1", "F"); ("2", "R"); ("3", "3"); ("7", "7") ];
     case "sample/hello.bfr"
       [ ("", "Hello World!\n") ];
     case "sample/misc/metalang/sort.bfr"
       [ ("", "34567") ];
-    case "sample/misc/hoge/switch.bfr"
+    case "sample/misc/bf/switch.bfr"
       [ ("+", "INC"); ("-", "DEC");
         (".", "PUT"); (",", "GET");
         ("[", "WHILE"); ("]", "WEND");
@@ -42,9 +42,9 @@ let cases =
       ];
     case "sample/misc/metalang/import_a.bfr"
       [ ("", "BC123") ];
-    case "sample/misc/hoge/nested_array.bfr"
+    case "sample/misc/bf/nested_array.bfr"
       [ ("", "XYPQqRSrs012MNmn") ];
-    case "sample/misc/hoge/queens.bfr" ~cell_type:WrapAround256
+    case "sample/misc/bf/queens.bfr" ~cell_type:WrapAround256
       [ ("2\n", "NO ANSWER\n");
         ( "4\n",
           ". Q . . \n" ^
@@ -66,7 +66,7 @@ let cases =
       [ ("A", "A"); ("B", "") ];
     case "sample/lib/counter.bfr"
       [ ("A", "65"); ("0", "48"); ("{", "123") ];
-    case "sample/misc/hoge/counter_test.bfr"
+    case "sample/misc/bf/counter_test.bfr"
       [ ("", "0\n123\n0\n4") ];
     case "sample/lib/fixedint.bfr"  ~run_bfi:false
       [ ("0 0\n", "0 0");
@@ -75,7 +75,7 @@ let cases =
         ("3 -4\n", "-1 -12");
         ("-5 -6\n", "-11 30");
       ];
-    case "sample/misc/hoge/addsubmul.bfr" ~run_bfi:false
+    case "sample/misc/bf/addsubmul.bfr" ~run_bfi:false
       [ ("3 1\n", "4\n");
         ("4 -2\n", "6\n");
         ("0 0\n", "0\n");
