9b0d5236597246dfcf49ebca4132a4d50c4e538d
rodoni <41773436+roodni@users.noreply.github.com>
2023-07-04 10:45:03 +0900

    マージされたメンバ名の出力時にマージ元のidを表示しない

--- lib/ir/id.ml
+++ lib/ir/id.ml
@@ -7,22 +7,16 @@ type label =
   | Named of string
   | Merged of t list
 
-let num = ref 0
-let nametable = Hashtbl.create 30
+let to_int (t: t): int = t
+let compare = Int.compare
 
-let gen label: t =
+let num = ref 0
+let nametable = Hashtbl.create 100
+let gen label : t =
   incr num;
   Hashtbl.add nametable !num label;
   !num
 
-let gen_special name = gen (Special name)
-let gen_named name = gen (Named name)
-let gen_merged l =
-  assert (l <> []);
-  gen @@ Merged l
-
-
-module SMap = Map.Make(String)
 
 let number_prefix = "%"
 
@@ -37,38 +31,28 @@ let numbered_name id =
   | Special s -> s
   | Named n -> sprintf "%s%s%d" n number_prefix id
   | Merged _ -> number_only_name id
-let numbered_names l =
+
+
+module SSet = Set.Make(String)
+let rec names l =
   l
   |> List.fold_left
-    (fun (sm: t list SMap.t) (id: t) ->
-      let name = match Hashtbl.find nametable id with
-        | Special s -> s
-        | Named n -> n
-        | Merged _ -> number_only_name id
-      in
-      let l = SMap.find_opt name sm |> Option.value ~default:[] in
-      SMap.add name (id :: l) sm
+    (fun set id -> match Hashtbl.find nametable id with
+      | Special s -> SSet.add s set
+      | Named n -> SSet.add n set
+      | Merged l -> SSet.add_seq (names l) set
     )
-    SMap.empty
-  |> SMap.to_seq
-  |> Seq.map
-    (fun (name, ids: string * t list): string ->
-      match ids with
-      | [id] -> sprintf "%s%s%d" name number_prefix id
-      | _ ->
-        let ids_s =
-          List.sort (Fun.flip Int.compare) ids
-          |> List.rev_map string_of_int
-          |> String.concat ","
-        in
-        sprintf "%s%s(%s)" name number_prefix ids_s
-    )
-  |> List.of_seq |> String.concat ", "
+    SSet.empty
+  |> SSet.to_seq
 let detailed_name id =
   match Hashtbl.find nametable id with
   | Special _ | Named _ -> numbered_name id
-  | Merged l -> sprintf "{%s}%s" (numbered_names l) (number_only_name id)
+  | Merged l ->
+      let names = names l |> List.of_seq |> String.concat ", " in
+      sprintf "{%s}%s" names (number_only_name id)
 
-let to_int (t: t): int = t
-
-let compare = Int.compare
+let gen_special name = gen (Special name)
+let gen_named name = gen (Named name)
+let gen_merged l =
+    assert (l <> []);
+    gen (Merged l)
